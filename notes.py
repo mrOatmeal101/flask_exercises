2bd9de814f2f5ea2ad88cab15165ddeb3eff4cb9
mroatmeal <jfeind101@gmail.com>
2024-11-04 12:47:54 -0600

    end of section 1

---
+++ notes.py
@@ -0,0 +1,35 @@
+# Section 1 Flask Introduction Handout
+# Whatever company you work at will want you to know a Python development framework. 
+# Flask is one of the two most popular Python web frameworks, and it’s far more user-friendly than the other contender Django. 
+# Reddit, Netflix, Lyft, Pinterest, and Airbnb all use Flask in their stack.
+
+# If you want to build a web application, you need to use a framework to do that. 
+# Web frameworks are a collection of the foundational tools necessary for web development. 
+# Flask maps URLs to the code they are requesting, connects to a database, creates and serves up customizable HTML templates, 
+# stores state information in a session, provides testing and debugging tools, and much much more. 
+
+# In this subunit, you'll learn how to use the Flask framework. 
+# Flask is what is called a microframework, which means that it provides more minimalistic functionality than an enterprise framework.
+
+# Section 2
+
+# Section 3
+
+# Section 4
+
+# Section 5
+
+# Section 6
+
+# Section 7
+
+# Section 8
+
+# Section 9
+
+# Section 10
+
+# Section 11
+
+# Section 12
+
