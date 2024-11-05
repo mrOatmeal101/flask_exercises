214a5ada9a5547a06e9ddd594992411410b242ee
mroatmeal <jfeind101@gmail.com>
2024-11-05 09:24:58 -0600

    end of section 3)

--- notes.py
+++ notes.py
@@ -22,7 +22,30 @@
 # Handle GET and POST requests with Flask
 # Extract data from different parts of the URL with Flask
 
-# Section 3
+# Section 3 Introduction to Web Servers
+# Flask will be your first back-end web framework.
+#  Web Frameworks
+# A Quick Demo
+    # (venv) $ FLASK_ENV=development flask run
+    # * Environment: development
+    # * Debug mode: on
+    # * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+    # * Restarting with stat
+    # * Debugger is active!
+    # * Debugger PIN: 160-080-703
+
+# What is a Web Server?
+# A program that’s running on a machine and waiting for a web request.
+# Note: A web server is a technology that can process requests and issue responses via HTTP, a protocol used to distribute 
+# information on the world wide web. Though it’s also used to refer to computer systems and even internet “appliances,” we’ll 
+# use web server to refer to the software running on a machine that’s waiting to respond to HTTP requests.
+
+# Request
+# web browser --GET/--> Web Server (app.py)
+# The server then responds with the exact HTML text for that page:
+
+# Response
+# Web Browser <--String of HTML <html>..<html> -- Web Server (app.py)
 
 # Section 4
 
