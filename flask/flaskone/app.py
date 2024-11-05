0e25bf9da7174d6ddcd400f9ed3cb428c9b484c3
mroatmeal <jfeind101@gmail.com>
2024-11-05 10:31:56 -0600

    end of section 5

---
+++ flask/flaskone/app.py
@@ -0,0 +1,9 @@
+# Step one to create a web server with Flask is to import it.
+from flask import Flask
+
+# next step is to make a variable called app and set it equal to Flask(__name__)
+app = Flask(__name__)
+
+# This is telling python to let Flask run in this file.
+# In the terminal then type | flask run | then the server will start
+# CTRL + C is how to quit the server. 
--- notes.py
+++ notes.py
@@ -78,7 +78,47 @@
     # cache pages for performance
     # & more!
 
-# Section 5
+# Section 5 Our First Server
+# Flask is a lightweight Python framework for web applications that provides the basics for URL routing and page rendering.
+
+# Flask is called a "micro" framework because it doesn't directly provide features like form validation, 
+# database abstraction, authentication, and so on. Such features are instead provided by special Python packages called Flask extensions. 
+# The extensions integrate seamlessly with Flask so that they appear as if they were part of Flask itself. 
+# For example, Flask doesn't provide a page template engine, but installing Flask includes the Jinja templating engine by default.
+# For convenience, we typically speak of these defaults as part of Flask.
+
+# In this video we first started a virtual enviornment inside a new folder called flask. 
+# did this by typing | python3 -m venv venv | this creates the folder venv and makes it a virtual enviroment.
+# Activated it by typing | source venv/bin/activate |
+# Then installed flask in the virtual enviorment with the command | pip install flask |
+# Made a file called app.py beacause that is the name that flask is expecting for a file name. 
+    # you can give it another name but you will have to edit things. 
+
+# In the file app.py we typed this. 
+# Step one to create a web server with Flask is to import it.
+# from flask import Flask
+
+# next step is to make a variable called app and set it equal to Flask(__name__)
+# app = Flask(__name__)
+
+# This is telling python to let Flask run in this file.
+# In the terminal then type | flask run | then the server will start
+# CTRL + C is how to quit the server. 
+# by default, the server, is in production mode and debug is off. 
+# by copying and pasting http://127.0.0.1:5000 which was created after we typed flask run,
+# into the web browser we will get the Not Found page with: 
+# The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.
+
+# the terminal will also show that there were request to the server:
+# 127.0.0.1 - - [05/Nov/2024 10:10:44] "GET / HTTP/1.1" 404 -
+# 127.0.0.1 - - [05/Nov/2024 10:10:44] "GET /favicon.ico HTTP/1.1" 404 -
+
+# if you ctrl+c out of the server and try to visit in browser, it will now show the site can't be reached.
+
+# If your Flask app file isn’t called app: 
+# (venv) $ FLASK_APP=app.py flask run
+# FLASK_APP=app.py is passing an “environmental variable”
+# Only has this meaning while this program is running
 
 # Section 6
 
