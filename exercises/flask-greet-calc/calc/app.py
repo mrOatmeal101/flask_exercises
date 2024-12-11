3c99604d584b2b0f17fa9435cfecd8f8051ff261
mroatmeal <jfeind101@gmail.com>
2024-12-11 10:24:54 -0600

    end of math exercise

--- exercises/flask-greet-calc/calc/app.py
+++ exercises/flask-greet-calc/calc/app.py
@@ -4,7 +4,7 @@
 # followed by creating the app.py file with: touch app.py
 
 # first line of code is to import Flask from the module flask 
-from flask import Flask
+from flask import Flask, request
 from operations import add, sub, mult, div
 
 # to create the flask application or instantiate it with app = Flask(__name__)
@@ -18,26 +18,102 @@ app = Flask(__name__)
 # Set export FLASK EVN=development in my bash profile. 
 # Also installed the debugger under the requirements.
 
+# needed to acess information from the query string, which in this case a = 10 & b= 20.
+# starts with ? and has key value pairs combined with & 
+# ?a=10&b=20
+# imported request the built in object from flask library.
 @app.route('/')
 def home_page():
-    return "hello"
+    return f"<h1>Home Page</h1>"
 
-# now making the four different routes
+@app.route('/search')
+def search():
+    term = request.args['term']
+    return f"<h1>Search for: {term}</h1>"
+
+# now making the four different routes which are all GET request. 
+# to use request, type request.args['name_of_term']
 @app.route('/add')
-def addition(a,b):
-    return add(10,30)
+def addition():
+    """Adding keyword arguments a and b."""
+    # need to turn a and b into intergers, otherwise will be read as strings
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    total = add(a,b)
+    return f'{total}'
 
 @app.route('/sub')
-def sub(a, b):
-    """Substract b from a."""
-    return a - b
+def subtraction():
+    """Subtracting keyword arguments a and b."""
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    total = sub(a,b)
+    return f'{total}'
 
 @app.route('/mult')
-def mult(a, b):
-    """Multiply a and b."""
-    return a * b
+def multiply():
+    """Multipling keyword arguments a and b."""
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    total = mult(a,b)
+    return f'{total}'
 
 @app.route('/div')
-def div(a, b):
-    """Divide a by b."""
-    return a / b
+def divide():
+    """Dividing keyword arguments a and b."""
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    total = div(a,b)
+    return f'{total}'
+
+# Now make a variable route so that the above routes can be compressed into one.
+# To do this take variable name and wrap in <>.
+# when you wrap the variable, flask takes it and plugs into function as a keyword argument.
+# the keyword argument needs to match exactly the wraped text. 
+# @app.route('/math/<operation>')
+# def math_op(operation):
+#     """Addition on keyword arguments a and b."""
+#     if operation == 'add':
+#         a = int(request.args['a'])
+#         b = int(request.args['b'])
+#         total = add(a,b)
+#         return f'{total}'
+    
+#     if operation == 'sub':
+#         """Subtracting keyword arguments a and b."""
+#         a = int(request.args['a'])
+#         b = int(request.args['b'])
+#         total = sub(a,b)
+#         return f'{total}'
+    
+#     if operation == 'mult':
+#         """Multipling keyword arguments a and b."""
+#         a = int(request.args['a'])
+#         b = int(request.args['b'])
+#         total = mult(a,b)
+#         return f'{total}'
+    
+#     if operation == 'div':
+#         """Dividing keyword arguments a and b."""
+#         a = int(request.args['a'])
+#         b = int(request.args['b'])
+#         total = div(a,b)
+#         return f'{total}'
+
+# without the if statements and pulling operations from a python dictionary
+ops = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div
+    }
+
+@app.route("/math/<operation>")
+def do_math(operation):
+    """Math operations on keyword arguments a and b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    total = ops[operation](a, b)
+
+    return f"{total}"
